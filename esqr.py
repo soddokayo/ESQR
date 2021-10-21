7350be5a7b4644ef96aec2caf16ad0c8b1aa567c
soddokayo <89368738+soddokayo@users.noreply.github.com>
2021-10-21 13:22:02 +0900

    Update esqr.py
    added header & footer for information bout file name, piece size, progress state, and error correction(sha1 hash)

--- esqr.py
+++ esqr.py
@@ -1,24 +1,55 @@
 # esqr.py
 
-import qrcode, sys, os
+import sys, os
+import qrcode
+import hashlib
 
-PSIZE = 1000
+PSIZE = 900
 
 if len(sys.argv)==2:
+	FNAME = sys.argv[1].split('\\')[-1]
+	print(sys.argv[1], FNAME)
+	
 	fin = open(sys.argv[1], "r", encoding="utf-8")
 	txt = fin.read()
 	
-	path = sys.argv[1] + ".result"
+	path = FNAME + ".result"
 	try:
 		os.mkdir(path)
 	except:
 		pass
 		
-	# slice into n-letter pieces
+	# slice into PSIZE-letter pieces
+	nn = int(len(txt)/PSIZE) + 1
+	sha1_hasher = hashlib.sha1()
+	sha1_hasher.update(txt.encode())
+	f_hash = sha1_hasher.hexdigest()
 	for i in range(0, len(txt), PSIZE):
-		img = qrcode.make(txt[i:i+PSIZE])
-		type(img)
-		foname = path + "/" + str(i) + "-" + str(i+PSIZE) + ".png"
-		img.save(foname)
+		rr = int(i/PSIZE) + 1
+
+		# header : text/binary, filename, psize, progress		
+		header = "?st t " + FNAME + " " + str(PSIZE) + " "\
+		+ str(rr) + "/" + str(nn) + "\n"
+			
+		# content : sliced PSIZE-letter data
+		content = txt[i:i+PSIZE]
+		
+		# footer : sha1 hash of piece/file
+		sha1_hasher = hashlib.sha1()
+		sha1_hasher.update(content.encode())
+		p_hash = sha1_hasher.hexdigest()
+		footer = "\n" + "?ed p " + str(p_hash)
+
+		if rr == nn:
+			footer += "\n" + "?ed f " + str(f_hash)
+
+		buf = header + content + footer
+	
+		print("\n===\n" + "processing " + str(rr) + "/" + str(nn))
+		print("\n" + buf)
+		
+		enc_img = qrcode.make(buf)
+		fo_name = path + "/" + str(i) + "-" + str(i+PSIZE) + ".png"
+		enc_img.save(fo_name)
 else:
 	print("Usage: python esqr.py [INPUT_FILENAME]")
